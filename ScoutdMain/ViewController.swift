c0efbe325b11c08dbaceb136b691ab8f4faed2d1
Danny Solis <527483@roc-134-s01nb.local>
2019-10-09 16:07:07 -0700

    Initial Commit

---
+++ ScoutdMain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScoutdMain
+//
+//  Created by Danny Solis on 10/9/19.
+//  Copyright © 2019 Danny Solis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
